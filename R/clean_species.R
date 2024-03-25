8fa720a4e95c6160b0c5ec55ee0f11ff3e3b9782
geryan <geryan@users.noreply.github.com>
2024-03-25 15:16:22 +1100

    clean species

---
+++ R/clean_species.R
@@ -0,0 +1,106 @@
+clean_species <- function(x){
+
+    # correct   from ~ to
+    case_when(
+      x == "carnevalai" ~ "carnevalei",
+      x == "d'thali" ~ "dthali",
+      x == "gambiae _ coluzzii"~ "coluzzi",
+      x == "gambiae (S_M)" ~ "gambiae",
+      x == "gambiae (S/M)" ~ "gambiae",
+      x == "marshalii"~ "marshallii",
+      x == "marshalli"~ "marshallii",
+      x == "mouchetti"~ "moucheti",
+      x == "rupicola" ~ "rupicolus",
+      x == "squamosus-cydippis"~ "cydippis",
+      # not confident in the above interpretation but low numbers so will be
+      # excluded anyway in sdm analysis
+      x == "squasmous "~ "squamosus",
+      x == "ziemani"~ "ziemanni",
+      x == "wilconi" ~ "wilsoni",
+      TRUE ~ x
+    )
+
+  # nb: this is just correcting the obvious duplicates but potentially not more
+  # subtle typos - e.g., wilconi was a lucky get. Antoinette / Marianne to go
+  # over final list
+
+  # "abscurus"
+  # "arabiensis"
+  # "ardensis"
+  # "barberellus"
+  # "brunnipes"
+  # "bwambae"
+  # "carnevalai"
+  # "carnevalei"
+  # "christyi"
+  # "chrysti"
+  # "cinereus"
+  # "claviger"
+  # "coluzzii"
+  # "coustani"
+  # "cydippis"
+  # "d'thali"
+  # "demeilloni"
+  # "domicola"
+  # "dthali"
+  # "flavicosta"
+  # "freetownensis"
+  # "funestus"
+  # "gambiae"
+  # "gambiae _ coluzzii"
+  # "gambiae (S_M)"
+  # "gambiae (S/M)"
+  # "GAMBIAE COMPLEX"
+  # "garnhami"
+  # "gibbinsi"
+  # "grassei"
+  # "hancocki"
+  # "harperi"
+  # "implexus"
+  # "labranchiae"
+  # "leesoni"
+  # "longipalpis"
+  # "maculipalpis"
+  # "marshalii"
+  # "marshalli"
+  # "marshallii"
+  # "mascarensis"
+  # "melas"
+  # "merus"
+  # "moucheti"
+  # "mouchetti"
+  # "multicolor"
+  # "namibiensis"
+  # "nili"
+  # "obscurus"
+  # "ovengensis"
+  # "paludis"
+  # "parensis"
+  # "pauliani"
+  # "pharoensis"
+  # "pretoriensis"
+  # "pseudopunctipennis"
+  # "quadriannulatus"
+  # "quadrimaculatus"
+  # "rhodesiensis"
+  # "rivulorum"
+  # "rufipes"
+  # "rupicola"
+  # "rupicolus"
+  # "sergentii"
+  # "smithii"
+  # "squamosus"
+  # "squamosus-cydippis"
+  # "squasmous"
+  # "stephensi"
+  # "swahilicus"
+  # "tenebrosus"
+  # "theileri"
+  # "vaneedeni"
+  # "wellcomei"
+  # "wilconi"
+  # "ziemani"
+  # "ziemanni"
+  # NA
+
+}
--- R/pa_from_va.R
+++ R/pa_from_va.R
@@ -31,28 +31,24 @@ pa_from_va <- function(va_data){
       pa = ifelse(any(presence == 0), "pa", "po")
     )|>
     ungroup() |>
-    select(source_ID, species, lon, lat, binary.presence, binary.absence, starts_with("n_"), count, presence, pa, starts_with("sampling.method_")) |>
-    arrange(source_ID, species)
+    select(
+      #source_ID,
+      species,
+      lon,
+      lat,
+      #binary.presence,
+      #binary.absence,
+      #starts_with("n_"),
+      #count,
+      presence,
+      pa#,
+      #starts_with("sampling.method_")
+    ) |>
+    mutate(
+      species = clean_species(species)
+    ) |>
+    arrange(species, pa, presence) |>
+    distinct()
+
 }
 
-
-#
-# x <- va_data |>
-#   filter(source_ID == 386, species == "GAMBIAE COMPLEX") %>%
-#   select(year_st, lon, lat, starts_with("sampling.method_"), starts_with("n_")) |>
-#   mutate(
-#     any_sm_na_count = any(
-#       !is.na(sampling.method_1) & is.na(n_1),
-#       !is.na(sampling.method_2) & is.na(n_2),
-#       !is.na(sampling.method_3) & is.na(n_3),
-#       !is.na(sampling.method_4) & is.na(n_4)
-#     ), # this checks if there are any non-empty sampling methods with a NA count
-#     all_sm_na = all(is.na(c_across(starts_with("sampling.method"))))
-#   ) |>
-#   rename(entered_n_tot = n_tot) |> # renaming becase want to keep for checking but will get double-counted by the sum(c_across(...)) below if left with a name beginning "n_"
-#   mutate(
-#     count = case_when(
-#       any_sm_na_count ~ NA, # assign NA n_tot if there is a non-empty sampling method that is NA
-#       TRUE ~ sum(c_across(starts_with("n_")), na.rm = TRUE) # otherwise sum up the values ignoring NAs
-#     )
-#   )
