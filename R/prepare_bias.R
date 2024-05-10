5ce1aa28bc9b9c4621ad4c3150ec2b1ccc62b548
geryan <geryan@users.noreply.github.com>
2024-05-10 16:18:19 +1000

    How heaven laughs out loud with glee

--- R/prepare_bias.R
+++ R/prepare_bias.R
@@ -1,6 +1,8 @@
-prepare_bias <- function(my_mask){
+prepare_bias <- function(my_mask, biaspath){
 
-  x <- terra::rast("~/Documents/tki_work/vector_atlas/vector_sdm_course/data/downloads/travel_time_to_cities_2.tif") |>
+  x <- terra::rast(
+   biaspath
+  ) |>
     terra::crop(my_mask) |>
     terra::mask(my_mask)
 
