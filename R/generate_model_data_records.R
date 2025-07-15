e07740dd7ba1821834698b5200b5d333015754fa
geryan <geryan@users.noreply.github.com>
2025-07-15 16:22:30 +1000

    A CRIME

--- R/generate_model_data_records.R
+++ R/generate_model_data_records.R
@@ -15,6 +15,16 @@ generate_model_data_records <- function(
 
   # full_data_records |>
   #   # target species only |>
-  #
+  #   filter(species %in% target_species) %$%
+  #   table(sampling_method, species)
+
+
+  full_data_records |>
+    # target species only |>
+    filter(species %in% target_species) |>
+    mutate(
+      sampling_method = reduce_sampling_methods(sampling_method)
+    )
+
 
 }
