28cdb3765b57d5bd9566f284000fa74b9709ef12
geryan <geryan@users.noreply.github.com>
2025-07-21 16:46:33 +1000

    filter down to enforce quality controls on data points

--- R/generate_model_data_records.R
+++ R/generate_model_data_records.R
@@ -22,8 +22,22 @@ generate_model_data_records <- function(
   full_data_records |>
     # target species only |>
     filter(species %in% target_species) |>
+    # fuck off vague points
+    filter(
+      (confidence_in_georef != "greater than 10km") |
+        is.na(confidence_in_georef)
+    ) |>
+    # fuck off polygons and centroid of admin points
+    filter(point_data) |>
     mutate(
-      sampling_method = reduce_sampling_methods(sampling_method),,
+      sampling_method = reduce_sampling_methods(sampling_method),
+    ) |>
+    select(
+      -start_date,
+      -confidence_in_georef,
+      -point_data,
+      -start_date,
+      -end_date
     )
 
 
