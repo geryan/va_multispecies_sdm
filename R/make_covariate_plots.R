6db44a6a1878994c64bf99abcf10add4a1ccc629
geryan <geryan@users.noreply.github.com>
2025-07-28 16:42:31 +1000

    get rid of loads of shit plots and one smaller nice one

--- R/make_covariate_plots.R
+++ R/make_covariate_plots.R
@@ -1,88 +1,45 @@
 make_covariate_plots <- function(
-    spatial_values,
+    model_data_spatial,
     target_species,
-    model_notna_idx_pa,
-    model_notna_idx_po,
+    target_covariate_names,
+    offset_names,
     w = 3200,
     h = 2000
 ){
 
-  cvnames <- names(spatial_values)
+  cvnames <- c(offset_names, target_covariate_names)
 
-  ncvs <- length(cvnames)
-
-  nspp <- length(target_species)
-
-  idx_sp_pa <- model_notna_idx_pa
-  idx_sp_pa[,2] <- target_species[idx_sp_pa[,2]]
-
-  idx_sp_po <- model_notna_idx_po
-  idx_sp_po[,2] <- target_species[idx_sp_po[,2]]
-
-  for(i in 1:ncvs){
-
-    # pa plot
-    plotdatpa <- tibble(
-      species = idx_sp_pa[,2],
-      value = spatial_values[idx_sp_pa[,1], i]
+  p <- model_data_spatial |>
+    filter(
+      data_type != "bg",
+      presence != 0
+    ) |>
+    pivot_longer(
+      cols = all_of(cvnames),
+      names_to = "var",
+      values_to = "value"
+    ) |>
+    ggplot() +
+    geom_violin(
+      aes(
+        x = species,
+        y = value,
+        fill = species
+      )
+    ) +
+    facet_wrap(
+      ~ var,
+      scales = "free"
     )
 
-    paplot <- ggplot(plotdatpa) +
-      geom_violin(
-        aes(
-          x = species,
-          y = value,
-          fill = species
-        )
-      ) +
-      labs(
-        title = cvnames[i]
-      )
-
     ggsave(
-      filename = sprintf(
-        "outputs/figures/cov_violins/pa_%s.png",
-        cvnames[i]
-      ),
-      plot = paplot,
+      filename = "outputs/figures/cov_violins.png",
+      plot = p,
       width = w,
       height = h,
       units = "px"
     )
 
-    # po plot
-    plotdatpo <- tibble(
-      species = idx_sp_po[,2],
-      value = spatial_values[idx_sp_po[,1], i]
-    )
-
-    poplot <- ggplot(plotdatpo) +
-      geom_violin(
-        aes(
-          x = species,
-          y = value,
-          fill = species
-        )
-      ) +
-      labs(
-        title = cvnames[i]
-      )
-
-    ggsave(
-      filename = sprintf(
-        "outputs/figures/cov_violins/po_%s.png",
-        cvnames[i]
-      ),
-      plot = poplot,
-      width = w,
-      height = h,
-      units = "px"
-    )
-
-  }
-
-
-
-
+  NULL
 
 }
