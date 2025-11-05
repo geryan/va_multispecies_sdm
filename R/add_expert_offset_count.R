a2cb052d1ffc73ed2d30ec9b1c936f42c2afc97e
geryan <geryan@users.noreply.github.com>
2025-11-05 17:33:40 +1100

    count with expert offset - this is a hack and need to integrate this pipeline better

---
+++ R/add_expert_offset_count.R
@@ -0,0 +1,58 @@
+#' .. content for \description{} (no empty lines) ..
+#'
+#' .. content for \details{} ..
+#'
+#' @title
+#' @param predfilelist
+#' @param expert_offset_maps
+#' @return
+#' @author geryan
+#' @export
+add_expert_offset_count <- function(
+    predfilelist,
+    expert_offset_maps
+  ) {
+
+  predraw <- rast(predfilelist$count)
+
+
+  # it would be nice to do this is a more sophisticated programmatic way
+  # but for now hard-coding for the win
+  # species_lookup <- list(
+  #   arabiensis = "arabiensis",
+  #   gambiae = c("coluzzii", "gambiae", "gambiae_coluzzii", "gambiae_complex"),
+  #   funestus = c("funestus", "funestus_complex"),
+  #   melas = "melas",
+  #   merus = "merus",
+  #   moucheti = "moucheti",
+  #   nili = "nili"
+  # )
+  #
+
+
+  r <- c(
+    terra::subset(predraw, "arabiensis") * expert_offset_maps$arabiensis,
+    terra::subset(predraw, "coluzzii") * expert_offset_maps$gambiae,
+    terra::subset(predraw, "funestus") * expert_offset_maps$funestus,
+    terra::subset(predraw, "gambiae") * expert_offset_maps$gambiae,
+    terra::subset(predraw, "melas") * expert_offset_maps$melas,
+    terra::subset(predraw, "merus") * expert_offset_maps$merus,
+    terra::subset(predraw, "moucheti") * expert_offset_maps$moucheti,
+    terra::subset(predraw, "nili") * expert_offset_maps$nili
+  )
+
+  filename <- sub(
+    pattern = "\\.tif",
+    replacement = "_expoff.tif",
+    x = predfilelist$count
+  )
+
+  writeRaster(
+    x = r,
+    filename = filename,
+    overwrite = TRUE
+  )
+
+  filename
+
+}
