17961c2972cd1b1074b8cb9e4638c69008adfb25
geryan <geryan@users.noreply.github.com>
2026-05-25 17:09:43 +1000

    spartan model sets structure plus m1

---
+++ spartan_model_comparison/run_m1.R
@@ -0,0 +1,65 @@
+# run m1
+
+# spartan stuff
+.libPaths("/home/ryange/R/gr_lib")
+
+n_burnin  <- 100
+n_samples <- 100
+n_chains  <- 50
+n_sims    <- 100
+
+model_name <- "m1"
+
+model_dir <- sprintf(
+  "spartan_model_comparison/%s",
+  model_name
+)
+
+
+library(targets)
+tar_load_globals()
+tar_load_everything()
+
+m1_fit <-fit_model_multisp_pp_count_sm(
+  model_data_spatial = model_data_spatial,
+  target_covariate_names = target_covariate_names,
+  target_species = target_species,
+  subrealm_names = subrealm_names,
+  bioregion_names = bioregion_names,
+  soiltype_names = soiltype_names,
+  project_mask = project_mask_5,
+  image_name = sprintf(
+    "%s/%s.RData",
+    model_dir,
+    model_name
+  ),
+  n_burnin = n_burnin,
+  n_samples = n_samples,
+  n_chains = n_chains
+)
+
+resids_multisp_pp_count_sm <- validation_and_checking(
+  m1_fit,
+  nsims = n_sims,
+  plotdir = model_dir
+)
+
+
+pred_lambda <- predict_lambda(
+  image_name = m1_fit,
+  prediction_layer = covariate_rast_10, # use 10k for faster preds
+  target_species,
+  output_file_prefix = model_dir,
+  offset = offsets_avg_10,
+  sm = TRUE, # if predict survey method
+  nsims = n_sims # lower for faster preds
+)
+
+pred_dist_sm <- rast(pred_lambda$p)
+
+
+distribution_plots_sm <-make_distribution_plots(
+  pred_dist_sm,
+  model_data_spatial,
+  plot_dir = model_dir
+)
