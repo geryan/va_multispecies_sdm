831e7ac71e397153ed5d81bfe9e304435b29c7da
geryan <geryan@users.noreply.github.com>
2026-06-16 14:06:18 +1000

    tidy run m6

--- spartan_model_comparison/run_m6.R
+++ spartan_model_comparison/run_m6.R
@@ -1,12 +1,12 @@
-# run m1
+# run m6
 
 # spartan stuff
 #.libPaths("/home/ryange/R/gr_lib")
 .libPaths("~/R/gr_lib")
 
 
-n_burnin  <- 2000
-n_samples <- 1000
+n_burnin  <- 20000
+n_samples <- 2000
 n_chains  <- 50
 n_sims    <- 100
 
@@ -55,25 +55,36 @@ resids_m6 <- validation_and_checking(
 )
 
 
-pred_lambda <- predict_lambda_m6_with_masking(
+pred_lambda_5 <- predict_lambda_m6_with_masking(
   image_name = m6_fit,
-  prediction_layer = covariate_rast_10, # use 10k for faster preds
+  prediction_layer = covariate_rast_5, # use 10k for faster preds
   target_species,
   output_file_prefix = sprintf(
     "%s/%s_mask",
     model_dir,
     model_name
   ),
-  offset = offsets_avg_10,
+  offset = offsets_avg_5,
   sm = TRUE, # if predict survey method
-  nsims = n_sims # lower for faster preds
+  nsims = n_sims, # lower for faster preds
+  bioregion_mask = bioregion_mask_5 * (1 - landcover_bare)
 )
 
 pred_dist_sm <- rast(pred_lambda$p)
 
+p <- add_expert_offset(pred_dist_sm, expert_offset_maps_10)
+
 
 distribution_plots_sm <- make_distribution_plots(
-  pred_dist_sm,
+  p,
   model_data_spatial,
   plot_dir = model_dir
 )
+
+writeRaster(
+  p,
+  10
+)
+
+
+
